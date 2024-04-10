352bd96d0e843128cfb65c72d4b4d18a206bbea3
Abraham <abrahamnhier97@gmail.com>
2024-04-10 11:12:17 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center text-teal-400">Eat train sleep</h1>
+   </div>
+  );
+}
+
+export default App;
