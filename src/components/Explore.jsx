1a65cf21a824151de2ceb6d3be7c297aadee4b7b
Abraham <abrahamnhier97@gmail.com>
2024-04-16 11:25:23 +0300

    improved styling

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -6,13 +6,13 @@ const Explore = () => {
   return (
     <div className="explore-container mt-10">
     <h2 className="mb-8 md:text-[5rem] text-4xl text-center">Programs for <span className="text-red-500">You</span></h2>
-    <div className="services-container">
+    <div className="flex flex-wrap md:gap-4 gap-1">
       {services.map((service, index) => (
-        <div key={index} className="service-item bg-gray-800">
-          <div className="service-content">
-            <img src={service.icon} alt={service.alt} className="service-icon" />
-            <h3 className="text-2xl p-4">{service.title}</h3>
-            <p className="text-lg">{service.description}</p>
+        <div key={index} className="service-item bg-gray-800 ">
+          <div className="p-7 text-center">
+            <img src={service.icon} alt={service.alt} className="w-14 h-14 object-cover m-auto" />
+            <h3 className="text-2xl p-4 text-white">{service.title}</h3>
+            <p className="text-lg text-[#ccc]">{service.description}</p>
           </div>
         </div>
       ))}
