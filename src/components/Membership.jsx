43496408d537f55be17b0597ef093244894d1911
Abraham <abrahamnhier97@gmail.com>
2024-04-19 08:25:48 +0300

    media queries

--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Membership = () => {
   return (
     <div className="mx-auto px-4 py-8">
-                <h1 className="text-4xl font-bold text-purple-800 text-center mb-8">Choose Your Membership Plan</h1>
+                <h1 className="text-3xl md:text-5xl font-bold text-purple-700 text-center mb-8">Choose Your Membership Plan</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                <div className="bg-gray-400 p-6 shadow-md rounded-lg">
-                    <h2 className="text-xl font-semibold mb-2 text-purple-800">Basic</h2>
+                <div className="bg-gray-800 p-6 shadow-md rounded-lg">
+                    <h2 className="text-xl font-semibold mb-2 text-purple-700">Basic</h2>
                     <ul>
                         <li className="text-gray-200">Basic Training plans</li>
                         <li className="text-gray-200">Personalized meal plans</li>
@@ -19,22 +19,22 @@ const Membership = () => {
                     <p className="text-purple-600 font-bold">$9.99/month</p>
                     <button className="mt-4 w-full bg-purple-600 text-white py-2 px-4 rounded-md hover:bg-purple-700 transition duration-300 ease-in-out">Select Plan</button>
                 </div>
-                <div className="bg-gray-400 p-6 shadow-md rounded-lg">
-                    <h2 className="text-xl font-semibold mb-2 text-purple-800">Premium</h2>
+                <div className="bg-gray-800 p-6 shadow-md rounded-lg">
+                    <h2 className="text-xl font-semibold mb-2 text-purple-700">Premium</h2>
                     <ul>
-                        <li className="text-gray-200">Basic training plans</li>
-                        <li className="text-gray-200">Personalized meal plans</li>
-                        <li className="text-gray-200">Monthly newsletter</li>
-                        <li className="text-gray-200">Exclusive fitness challenges</li>
-                        <li className="text-gray-200">Access to progess tracking features</li>
+                        <li className="text-[#ccc]">Basic training plans</li>
+                        <li className="text-[#ccc]">Personalized meal plans</li>
+                        <li className="text-[#ccc]">Monthly newsletter</li>
+                        <li className="text-[#ccc]">Exclusive fitness challenges</li>
+                        <li className="text-[#ccc]">Access to progess tracking features</li>
                         <li className="line-through">Monthly online coaching</li>
                         <li className="line-through">Personalized workout plans</li>
                     </ul>
                     <p className="text-purple-600 font-bold">$19.99/month</p>
                     <button className="mt-4 w-full bg-purple-600 text-white py-2 px-4 rounded-md hover:bg-purple-700 transition duration-300 ease-in-out">Select Plan</button>
                 </div>
-                <div className="bg-gray-400 p-6 shadow-md rounded-lg">
-                    <h2 className="text-xl font-semibold mb-2 text-purple-800">Elite</h2>
+                <div className="bg-gray-800 p-6 shadow-md rounded-lg">
+                    <h2 className="text-xl font-semibold mb-2 text-purple-700">Elite</h2>
                     <ul>
                         <li className="text-gray-200">Basic training plans</li>
                         <li className="text-gray-200">Personalized meal plans</li>
--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -4,16 +4,16 @@ import { trainer } from '../assets';
 const Trainers = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="bg-gray-800 rounded-md overflow-hidden shadow-lg text-center">
+      <div className="bg-gray-800 rounded-md overflow-hidden shadow-lg m-auto">
         <div className="p-8">
-          <h2 className="text-4xl font-bold text-purple-800 mb-4">Meet Our Trainers</h2>
+          <h2 className="text-3xl md:text-4xl font-semibold text-purple-700 mb-4">Meet Our Trainers</h2>
           <p className="text-lg text-gray-300 mb-8">Get trained by experienced professionals</p>
-          <div className="flex items-center justify-center">
-            <img src={trainer} alt="trainer" className="w-[200px] h-[200px] rounded-lg object-cover mr-4" />
+          <div className="flex items-center">
+            <img src={trainer} alt="trainer" className="w-[200px] h-[200px] rounded-lg object-cover mr-4 md:mr-1" />
             <div>
-              <h3 className="text-xl font-semibold text-white">John Doe</h3>
-              <p className="text-gray-300">Specialty: Strength Training</p>
-              <p className="text-gray-300">Experience: 10+ years</p>
+              <h3 className="text-xl md:text-sm font-semibold text-white text-center">John Doe</h3>
+              <p className="text-gray-300 text-center">Specialty: Strength Training</p>
+              <p className="text-gray-300 text-center">Experience: 10+ years</p>
             </div>
           </div>
         </div>
