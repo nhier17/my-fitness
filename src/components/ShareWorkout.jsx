9ee7b358b1f67a5563591d043f16b3fd0ddae57b
Abraham <abrahamnhier97@gmail.com>
2024-04-29 18:33:45 +0300

    share workout on socials

---
+++ src/components/ShareWorkout.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+    EmailShareButton,
+    FacebookShareButton,
+    InstapaperShareButton,
+    LinkedinShareButton,
+    PinterestShareButton,
+    RedditShareButton,
+    TwitterShareButton,
+ 
+  } from "react-share";
+
+const ShareWorkout = () => {
+    const shareUrl = 'https://my-fitness17.netlify.app'
+    const title = 'Check out my workout achievement!'
+  return (
+    <div>
+       <FacebookShareButton url={shareUrl} quote={title}>
+        Share on Facebook
+      </FacebookShareButton>
+      <TwitterShareButton url={shareUrl} title={title}>
+        Share on Twitter
+      </TwitterShareButton>
+      <LinkedinShareButton url={shareUrl} title={title}>
+        Share on LinkedIn
+      </LinkedinShareButton>
+      <RedditShareButton url={shareUrl} title={title}>
+        Share on Reddit
+      </RedditShareButton>
+      <PinterestShareButton url={shareUrl} title={title}>
+        Share on Pinterest
+      </PinterestShareButton>
+    </div>
+  )
+}
+
+export default ShareWorkout
